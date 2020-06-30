6d6bf1d2ae5bd90418d19c347602f1309e9448d7
Abhishek Vaid <abhishekvaid13@gmail.com>
2020-07-01 00:38:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def main():
 	"""
 	
 	gtml_temp = """
-	<iframe width="560" height="315" src="https://www.youtube.com/embed/vKjzSq7njLw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/6Y2kLF1IKEs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 	</div>
 	"""
 	
